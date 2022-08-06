e92e56cd400aa9592c8f375cb9e9ffd22305867d
YooGle <yooglesharma@gmail.com>
2022-08-06 14:44:38 +0530

    Particle Simulation Complete

--- projects/FireSimulator/FireSimulator/Particle.h
+++ projects/FireSimulator/FireSimulator/Particle.h
@@ -12,25 +12,44 @@
 #include <math.h>
 
 struct Particle {
-    double x;
-    double y;
+private:
     double speed;
     double angle;
 
+public:
+    double x;
+    double y;
+    
     Particle() {
+        init();
+    }
+    
+    void init() {
         x = 0;
         y = 0;
         
         angle = (2 * M_PI * rand()) / RAND_MAX;
-        speed = (0.0001 * rand()) / RAND_MAX;
+        speed = (0.04 * rand()) / RAND_MAX;
+        
+        speed *= speed;
     }
     
     void update(int interval) {
+        angle += interval * 0.0003;
+        
         double xSpeed = speed * cos(angle);
         double ySpeed = speed * sin(angle);
         
         x += xSpeed * interval;
         y += ySpeed * interval;
+        
+        if (x < -1 || x > 1 || y < -1 || y > 1) {
+            init();
+        }
+        
+        if (rand() < RAND_MAX/100) {
+            init();
+        }
     }
 };
 
